8ac75b76b7035cf22ef128b269c309c3e60888df
MehBirb <john.lu10212004@gmail.com>
2022-07-02 17:23:24 -0400

    Added Files
    I have an empty main file and more

---
+++ src/SudoSolveMain.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class SudoSolveMain{
+    //Properties
+
+
+    //Methods
+
+
+    //Constructor
+
+
+    //Main
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
