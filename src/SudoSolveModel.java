48def077c7180a6fbd8f8ccbd90d3191b1dba428
MehBirb <john.lu10212004@gmail.com>
2022-07-03 00:31:51 -0400

    Created process of elimination methods

--- src/SudoSolveModel.java
+++ src/SudoSolveModel.java
@@ -2,8 +2,8 @@ public class SudoSolveModel {
     //Properties
     public int[][] intSudokuArray = new int[9][9];
     public boolean[] blnPossibleValues = SudoSolveUtilities.resetPossibilities();
-    public int[] intCoordinates = new int[2];
-    public int[] intBox = new int[2];
+    public int[] intChosenCoords = new int[2];
+    public int[] intBoxCoords = new int[2];
 
     //Methods
 
--- src/SudoSolveUtilities.java
+++ src/SudoSolveUtilities.java
@@ -8,11 +8,63 @@ public class SudoSolveUtilities {
         return blnResetArray;
     }
 
-    //use this to locate row and column
-    public static int[] locateBox(int[] intCoordinates){
-        int[] intBox = new int[2];
-        intBox[0] = (int)(Math.ceil(intCoordinates[0]/3));
-        intBox[1] = (int)(Math.ceil(intCoordinates[1]/3));
-        return intBox;
+    //Use this to locate row and column
+    public static int[] locateBox(int[] intChosenCoords){
+        int[] intBoxCoords = new int[2];
+        intBoxCoords[0] = (int)(Math.ceil(intChosenCoords[0]/3));
+        intBoxCoords[1] = (int)(Math.ceil(intChosenCoords[1]/3));
+        return intBoxCoords;
     }
+
+    //Use this method to check whether a number is in that box yet. Return value if it's there or not.
+    public static boolean[] eliminateBoxPossibilities(int[][] intSudokuArray, boolean[] blnPossibleValues, int[] intBoxCoords, int[] intChosenCoords){
+        for(int intRow = (intBoxCoords[0]-1) * 3 ; intRow < intBoxCoords[0]*3; intRow++){
+            for(int intClm = (intBoxCoords[1]-1) * 3 ; intClm < intBoxCoords[1]*3; intClm++){
+                //If in row x and clm y there's a value there already, then erase it off the list.
+                blnPossibleValues = falsifyPossibilities(intSudokuArray, blnPossibleValues, intRow, intClm);
+            }
+        }
+        return blnPossibleValues;
+    }
+
+    //Use this method to check whether a number is in that row yet.
+    public static boolean[] eliminateRowPossibilities(int[][] intSudokuArray, boolean[] blnPossibleValues, int[] intChosenCoords){
+        for(int intClm = 0; intClm<9;intClm++){
+            blnPossibleValues = falsifyPossibilities(intSudokuArray, blnPossibleValues, intChosenCoords[0], intClm);
+        }
+        return blnPossibleValues;
+    }
+
+    //Use this method to check whether a number is in that column yet.
+    public static boolean[] eliminateClmPossibilities(int[][] intSudokuArray, boolean[] blnPossibleValues, int[] intChosenCoords){
+        for(int intRow = 0; intRow<9;intRow++){
+            blnPossibleValues = falsifyPossibilities(intSudokuArray, blnPossibleValues, intRow, intChosenCoords[1]);
+        }
+        return blnPossibleValues;
+    }
+
+    //Use this method to turn booleans false
+    public static boolean[] falsifyPossibilities(int[][] intSudokuArray, boolean[] blnPossibleValues, int intRow, int intClm){
+        if(intSudokuArray[intRow][intClm]==1){
+            blnPossibleValues[0] = false;
+        }else if(intSudokuArray[intRow][intClm]==2){
+            blnPossibleValues[1] = false;
+        }else if(intSudokuArray[intRow][intClm]==3){
+            blnPossibleValues[2] = false;
+        }else if(intSudokuArray[intRow][intClm]==4){
+            blnPossibleValues[3] = false;
+        }else if(intSudokuArray[intRow][intClm]==5){
+            blnPossibleValues[4] = false;
+        }else if(intSudokuArray[intRow][intClm]==6){
+            blnPossibleValues[5] = false;
+        }else if(intSudokuArray[intRow][intClm]==7){
+            blnPossibleValues[6] = false;
+        }else if(intSudokuArray[intRow][intClm]==8){
+            blnPossibleValues[7] = false;
+        }else if(intSudokuArray[intRow][intClm]==9){
+            blnPossibleValues[8] = false;
+        }
+        return blnPossibleValues;
+    }
+
 }
